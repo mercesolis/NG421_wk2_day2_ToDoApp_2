26c9731427b3f7386584bf991ae9d6a53b8712b6
mercesolis <mersolis@live.com>
2020-08-19 20:05:38 -0500

    add interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,31 @@
 import { Component,OnInit } from '@angular/core';
+import { ITodo } from './interfaces/itodo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Todos';
-  todoList: any [] = [];
-  todoTitle: string;
+export class AppComponent {
+  title = "Todos";
   todoId: number = 0;
-  ngOnInit() {
-    this.todoTitle = '';
-    this.todoList = [
-      // example of how to make an item in todo list
-      { title: 'Install Angular CLI', isDone: false },
-    
-    ];
-  }
+  todoList: ITodo [] = [];
+  todoTitle: string;
+
   addTodo():void {
     this.todoList.push({
+      id: this.todoId,
       title: this.todoTitle,
-      isDone: false
+      description: ''
     });
-    
-    // resets our todoTitle variable to an empty string
+  
     this.todoTitle = '';
+    this.todoId++;
+  
   }
-  deleteTodo(todo:any) {
+
+
+  deleteTodo(todo:ITodo) {
     const index = this.todoList.findIndex(todoItem => todoItem === todo);
     this.todoList.splice(index, 1);
   }
