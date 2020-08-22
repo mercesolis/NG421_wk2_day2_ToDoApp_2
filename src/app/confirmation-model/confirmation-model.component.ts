120960ea8953217c6ebbb65308d91b0933a61c3a
mercesolis <mersolis@live.com>
2020-08-22 09:22:26 -0500

    add ng bootstrap and confirmation modal

---
+++ src/app/confirmation-model/confirmation-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-model',
+  templateUrl: './confirmation-model.component.html',
+  styleUrls: ['./confirmation-model.component.css']
+})
+export class ConfirmationModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
